928ad1d2a4038a4811df8092d12bbe24663be004
BirdyXV <ethanmoore16@outlook.com>
2017-11-09 17:14:32 +1100

    Update ItemGen.cs
    YAYAYAYYAYA ESKEEEEEETIITT HAHAHAHAHHA SJASDAS

--- Assets/Scripts/Inv/ItemGen.cs
+++ Assets/Scripts/Inv/ItemGen.cs
@@ -35,10 +35,58 @@ public class ItemGen
                 break;
             #endregion
             #region Weapon 100-199
+                  case 0:
+                name = "Axe";
+                value = 50;
+                description = "Brutal Sharp Axe";
+                icon = "Axe";
+                mesh = "Axe";
+                type = ItemType.Weapon;
+                break;
+            case 1:
+                name = "Bow And Arrow";
+                value = 45;
+                description = "Shoot Down Your Enemies From Afar";
+                icon = "Bow And Arrow";
+                mesh = "Bow And Arrow";
+                type = ItemType.Weapon;
+                break;
             #endregion
             #region Apparel 200-299
+                     case 0:
+                name = "Cloak";
+                value = 20;
+                description = "Hide In The Shadows";
+                icon = "Cloak";
+                mesh = "Cloak";
+                type = ItemType.Apparel;
+                break;
+            case 1:
+                name = "Glove";
+                value = 5;
+                description = "Get A Better Grip On Your Weapons";
+                icon = "Glove";
+                mesh = "Glove";
+                type = ItemType.Apparel;
+                break;
             #endregion
             #region Crafting 300-399
+                       case 0:
+                name = "Diamond";
+                value = 1000;
+                description = "Shiny Diamond";
+                icon = "Diamond";
+                mesh = "Diamond";
+                type = ItemType.Crafting;
+                break;
+            case 1:
+                name = "Ingot";
+                value = 500;
+                description = "Upgrade Your Armour Immensely";
+                icon = "Ingot";
+                mesh = "Ingot";
+                type = ItemType.Crafting;
+                break;
             #endregion
             #region Quest 400-499
             #endregion
